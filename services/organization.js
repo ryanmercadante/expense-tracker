854cbe63b3ebcd8707c452b874327f6188324a08
Ryan Mercadante <ryan.a.mercadante@gmail.com>
2020-04-11 16:08:30 -0400

    setup auth so you must be signed in to create an organization

--- services/organization.js
+++ services/organization.js
@@ -1,10 +1,25 @@
+import { AuthenticationError } from 'apollo-server'
+import bcrypt from 'bcryptjs'
+
 import Organization from '../models/Organization'
 
 class OrganizationService {
-  static async createOrganization(name) {
+  static async createOrganization(name, password, user) {
     try {
+      // check for user
+      if (!user) {
+        throw new AuthenticationError(
+          'You must have a user account to create an organization',
+        )
+      }
+
+      // hash password
+      const hashedPassword = await bcrypt.hash(password, 12)
       const newOrganization = new Organization({
         name,
+        password: hashedPassword,
+        admin: user.id,
+        members: [user.id],
         createdAt: new Date().toISOString(),
       })
       const organization = await newOrganization.save()
